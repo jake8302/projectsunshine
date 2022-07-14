b4ed47919e46810785bbac6caf9bc2004f3ad3a5
jake wang <jake.wang@firsthx.com>
2022-07-14 16:47:53 -0400

    slider demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Typography, AppBar, CssBaseline, Toolbar, Dialog, DialogTitle, DialogContent, DialogContentText } from '@mui/material';
+import { Typography, AppBar, CssBaseline, Toolbar, Dialog, DialogTitle, DialogContent, DialogContentText, Box, Slider, Container } from '@mui/material';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import { ThemeProvider, createTheme, } from '@mui/material/styles';
 import { FhxThemeSelector } from './common/FhxContentComponents';
 import { FhxSelectOne } from './content/FhxSelectOne';
+import { styled } from "@mui/material/styles";
+
+
+function valuetext(value) {
+    return `${value}°C`;
+}
 
 const App = () => {
     const data = {
@@ -46,13 +52,169 @@ const App = () => {
     const [demoDialogText, setDemoDialogText] = React.useState(() => { });
     const handleClose = () => { setDemoDialog(false) };
 
+    const marks = [
+        {
+            value: 0,
+            label: '0 - No pain at all 😊',
+        },
+        {
+            value: 1,
+        },
+        {
+            value: 2,
+        },
+        {
+            value: 3,
+        },
+        {
+            value: 4,
+        },
+        {
+            value: 5,
+            label: '5 - moderate pain 😢',
+        },
+        {
+            value: 6,
+        },
+        {
+            value: 7,
+        },
+        {
+            value: 8,
+        },
+        {
+            value: 9,
+        },
+        {
+            value: 10,
+            label: '10 - Pain is unbearable 😱',
+        },
+    ];
+
+    const iOSBoxShadow =
+        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
+
+    const IOSSlider = styled(Slider)(({ theme }) => ({
+        color: theme.palette.mode === "dark" ? "#3880ff" : "#3880ff",
+        height: 2,
+        padding: "15px 0",
+        "& .MuiSlider-thumb": {
+            height: 28,
+            width: 28,
+            backgroundColor: "#fff",
+            boxShadow: iOSBoxShadow,
+            "&:focus, &:hover, &.Mui-active": {
+                boxShadow:
+                    "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
+                // Reset on touch devices, it doesn't add specificity
+                "@media (hover: none)": {
+                    boxShadow: iOSBoxShadow
+                }
+            }
+        },
+        "& .MuiSlider-valueLabel": {
+            fontSize: 12,
+            fontWeight: "normal",
+            top: -6,
+            backgroundColor: "unset",
+            color: theme.palette.text.primary,
+            "&:before": {
+                display: "none"
+            },
+            "& *": {
+                background: "transparent",
+                color: theme.palette.mode === "dark" ? "#fff" : "#000"
+            }
+        },
+        "& .MuiSlider-track": {
+            border: "none"
+        },
+        "& .MuiSlider-rail": {
+            opacity: 0.5,
+            backgroundColor: "#bfbfbf"
+        },
+        "& .MuiSlider-mark": {
+            backgroundColor: "#bfbfbf",
+            height: 8,
+            width: 1,
+            "&.MuiSlider-markActive": {
+                opacity: 1,
+                backgroundColor: "currentColor"
+            }
+        }
+    }));
 
     return (
         <>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
-                <FhxThemeSelector callback={changeTheme} />
-                <AppBar position='relative'>
+                <Container maxWidth="sm" sx={{ display: 'block', justifyContent: "center", alignItems: "center" }}>
+                    <FhxThemeSelector callback={changeTheme} />
+                    <Box display="block" justifyContent="center" alignItems="center" sx={{paddingBottom: 5}}>
+                        <Slider
+                            aria-label="Slider Demo"
+                            defaultValue={0}
+                            getAriaValueText={valuetext}
+                            valueLabelDisplay="auto"
+                            step={1}
+                            marks={marks}
+                            min={0}
+                            max={10}
+                            sx= {{
+                                ".MuiSlider-thumb": {
+                                    height: 27,
+                                    width: 27,
+                                    backgroundColor: "#fff",
+                                    border: "1px solid currentColor",
+                                    "&:hover": {
+                                      boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)"
+                                    },
+
+                                ".airbnb-bar": {
+                                    height: 9,
+                                    width: 1,
+                                    backgroundColor: "currentColor",
+                                    marginLeft: 1,
+                                    marginRight: 1
+                                    }  
+
+                            }}}
+                        />
+                    </Box>
+                    <Box display="block" justifyContent="center" alignItems="center" sx={{paddingBottom: 5}}>
+                    <IOSSlider
+                            aria-label="ios slider"
+                            defaultValue={0}
+                            getAriaValueText={valuetext}
+                            valueLabelDisplay="auto"
+                            step={1}
+                            marks={marks}
+                            min={0}
+                            max={10}
+                        />
+                    </Box>
+                    <Box display="block" justifyContent="center" alignItems="center" sx={{paddingBottom: 5}}>
+                        <Slider
+                            aria-label="Slider Demo"
+                            defaultValue={0}
+                            getAriaValueText={valuetext}
+                            valueLabelDisplay="auto"
+                            step={1}
+                            marks={marks}
+                            min={0}
+                            max={10}
+                            sx={{
+                                ".MuiSlider-mark": {
+                                    height: 8,
+                                    ".MuiSlider-markActive": {
+                                        backgroundColor: "currentColor"
+                                    }
+                                }
+                            }}
+                        />
+                    </Box>
+                </Container>
+                {/* <AppBar position='relative'>
                     <Toolbar>
                         <WbSunnyIcon style={{ marginRight: '20px' }} />
                         <Typography variant='h6' >
@@ -75,7 +237,7 @@ const App = () => {
                             {demoDialogText}
                         </DialogContentText>
                     </DialogContent>
-                </Dialog>
+                </Dialog> */}
             </ThemeProvider>
 
         </>
